23347b85de122245c4512878c6cf1f3d127aa254
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-04-02 18:18:43 +0900

    Create bunjang.py

---
+++ bunjang.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+def bunjang(word, pages):
+    file_path = "./bunjang_result/" + word + '.json'
+
+    pid = []
+
+    for page in range(pages):
+        url = 'https://api.bunjang.co.kr/api/1/find_v2.json?order=date&n=96&page={}&req_ref=search&q={}&stat_device=w&stat_category_required=1&version=4'.format(page, word)
+        response = requests.get(url)
+        datas = response.json()['list']
+        ids = [data['pid'] for data in datas]
+        pid.extend(ids)
+
+        items = []
+
+        for id in pid:
+            url = 'https://api.bunjang.co.kr/api/1/product/{}/detail_info.json?version=4'.format(id)
+            response = requests.get(url)
+            try:
+                details = response.json()['item_info']
+                details.pop('category_name')
+                details.pop('pay_option')
+                items.append(details)
+            except:
+                print('error')
+
+        df = pd.DataFrame(items)
+
+        bunjang_df = df[['name', 'price', 'num_item_view', 'pid']]
+        bunjang_df = bunjang_df.rename({'name':'title', 'num_item_view':'view_counts', 'link':'url'}, axis='columns')
+        bunjang_df['url'] = 'https://m.bunjang.co.kr/products/'+ bunjang_df['pid']
+        bunjang_df.drop(['pid'], axis=1)
+
+        bunjang = bunjang_df.to_dict("records")
+
+        with open(file_path, 'w', encoding='UTF-8-sig') as file:
+                file.write(json.dumps(bunjang, ensure_ascii=False, indent="\t"))
+
+        return bunjang
+
+if __name__ == "__main__":
+    bunjang(input(), 1)
