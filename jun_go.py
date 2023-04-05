093f4474e90d8d7e26b79dfd6181623bd900db13
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-04-05 20:13:28 +0900

    Update jun_go.py

--- jun_go.py
+++ jun_go.py
@@ -3,42 +3,14 @@ import streamlit as st
 import requests
 import json
 
-st.write('Hello World!')
+def activate_sidebar(df):
+    with st.sidebar:
+        uploaded_files = st.file_uploader('CSV 파일 혹은 ZIP 파일을 업로드해주세요.', accept_multiple_files=True)
+    # Check if files were uploaded
+    if len(uploaded_files) > 0:
+        if uploaded_file.type == 'text/csv':
+            uploaded_df = pd.read_csv(uploaded_file)
+            st.write(uploaded_df)
 
-def bunjang(word, pages):
-    # file_path = "./bunjang_result/" + word + '.json'
-
-    pid = []
-
-    for page in range(pages):
-        url = 'https://api.bunjang.co.kr/api/1/find_v2.json?order=date&n=96&page={}&req_ref=search&q={}&stat_device=w&stat_category_required=1&version=4'.format(page, word)
-        response = requests.get(url)
-        datas = response.json()['list']
-        ids = [data['pid'] for data in datas]
-        pid.extend(ids)
-
-        items = []
-
-        for id in pid:
-            url = 'https://api.bunjang.co.kr/api/1/product/{}/detail_info.json?version=4'.format(id)
-            response = requests.get(url)
-            try:
-                details = response.json()['item_info']
-                details.pop('category_name')
-                details.pop('pay_option')
-                items.append(details)
-            except:
-                print('error')
-
-        df = pd.DataFrame(items)
-
-        bunjang_df = df[['name', 'price', 'num_item_view', 'pid']]
-        bunjang_df = bunjang_df.rename({'name':'title', 'num_item_view':'view_counts', 'link':'url'}, axis='columns')
-        bunjang_df['url'] = 'https://m.bunjang.co.kr/products/'+ bunjang_df['pid']
-        bunjang_df.drop(['pid'], axis=1)
-
-        bunjang = bunjang_df.to_dict("records")
-
-        return bunjang_df
-
-st.dataframe(bunjang(input(), 1))
+df = dict()
+activate_sidebar(df)
