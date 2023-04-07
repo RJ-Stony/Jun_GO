3ec2f01489c886f86b10a7d4a9c4635bcf9ccd42
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-04-07 13:07:32 +0900

    Update jun_go.py

--- jun_go.py
+++ jun_go.py
@@ -2,18 +2,18 @@ import pandas as pd
 import streamlit as st
 import requests
 
-'''def activate_sidebar(df):
-    with st.sidebar:
-        uploaded_files = st.file_uploader('CSV 파일 혹은 ZIP 파일을 업로드해주세요.', accept_multiple_files=True)
+#def activate_sidebar(df):
+#    with st.sidebar:
+#        uploaded_files = st.file_uploader('CSV 파일 혹은 ZIP 파일을 업로드해주세요.', accept_multiple_files=True)
     # Check if files were uploaded
-    if len(uploaded_files) > 0:
-        for uploaded_file in uploaded_files:
-            if uploaded_file.type == 'text/csv':
-                uploaded_df = pd.read_csv(uploaded_file)
-                st.write(uploaded_df)
+#    if len(uploaded_files) > 0:
+#        for uploaded_file in uploaded_files:
+#            if uploaded_file.type == 'text/csv':
+#                uploaded_df = pd.read_csv(uploaded_file)
+#                st.write(uploaded_df)
 
-df = dict()
-activate_sidebar(df)'''
+#df = dict()
+#activate_sidebar(df)
 
 def bunjang(word, pages):
     # file_path = "./bunjang_result/" + word + '.json'
